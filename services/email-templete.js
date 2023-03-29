54fb73389a219ad063134e5f13924917bb4027f9
fares ramadan <fofas2000@yahoo.com>
2023-03-29 04:13:57 +0200

    changes

--- services/email-templete.js
+++ services/email-templete.js
@@ -194,7 +194,7 @@ export const html = (token, name) => {
                         <table border="0" cellpadding="0" cellspacing="0">
                           <tr>
                             <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                              <a href="http://localhost:3000/user/verify/${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Confirm Your Email</a>
+                              <a href="https://sarahahbackend.onrender.com/user/verify/${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Confirm Your Email</a>
                             </td>
                           </tr>
                         </table>
@@ -209,7 +209,7 @@ export const html = (token, name) => {
               <tr>
                 <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
                   <p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:</p>
-                  <p style="margin: 0;"><a href="http://localhost:3000/user/verify/${token}" target="_blank">http://localhost:3000/user/verify</a></p>
+                  <p style="margin: 0;"><a href="https://sarahahbackend.onrender.com/user/verify/${token}" target="_blank">http://localhost:3000/user/verify</a></p>
                 </td>
               </tr>
               <!-- end copy -->
